bc6caff53bbc7d5d7d3b33dae4f1cf070fd03deb
Kenta Moriguchi <nakaikenta@outlook.com>
2025-01-26 17:17:17 +0900

    update actions fillel

--- .github/scripts/notice_prs.py
+++ .github/scripts/notice_prs.py
@@ -1,4 +1,3 @@
-
 import os
 import requests
 import time
@@ -11,7 +10,7 @@ CONFIG_FILE_PATH = ".github/scripts/config.json"
 DEV_OPS_TOKEN = os.getenv("DEV_OPS_TOKEN")
 # PR情報を格納するためのファイル名
 ALL_PRS_FILE_NAME = "all_prs.json"
-FILTERED_PRS_FILE_NAME = "filtered_prs.json"
+REVIEWER_COUNT_LIMIT=2
 
 def load_configs():
     with open(CONFIG_FILE_PATH, "r") as file:
@@ -37,41 +36,27 @@ def filter_prs(prs, label):
         # ラベルとドラフト状態のフィルタリング
         if any(l["name"] == label for l in pr.get("labels", [])) and not pr.get("draft", True):
             # requested_reviewersが2件未満の場合はwaiting_prsに、それ以外はcomplete_prsに仕分け
-            if len(pr.get("requested_reviewers", [])) < 2:
+            if len(pr.get("requested_reviewers", [])) < REVIEWER_COUNT_LIMIT:
                 waiting_prs.append(pr["html_url"])
             else:
                 complete_prs.append(pr["html_url"])
     return waiting_prs, complete_prs
 
-def check_mergeable_state(pr_url):
-    max_retries = 5
-    retry_delay = 5
-    retries = 0
-
-    while retries < max_retries:
-        response = requests.get(pr_url, headers=HEADERS)
-        if response.status_code != 200:
-            print(f"Failed to fetch details for {pr_url}")
-            return None
-
-        pr_details = response.json()
-        mergeable_state = pr_details.get("mergeable_state")
-        if mergeable_state == "clean":
-            return "mergeable"
-        elif mergeable_state == "unknown":
-            print(f"mergeable_state is unknown. Retrying in {retry_delay} seconds...")
-            time.sleep(retry_delay)
-            retries += 1
-        else:
-            return "pending"
-    return "pending"
-
 def send_notification(waiting_prs: List[str], complete_prs: List[str], webhook_url: str):
     # メッセージを整形
     waitingForReviewTitle = ":heavy_check_mark: レビュー待ちのPR"
-    completeForReviewTitle = ":white_check_mark: レビューが完了しているPR"
-    text = f"{waitingForReviewTitle}\n" + "\n".join(waiting_prs) + f"\n{completeForReviewTitle}\n" + "\n".join(complete_prs)
+    if waiting_prs:
+        waitingPrsText = "\n".join(waiting_prs)
+    else:
+        waitingPrsText = "レビュー待ちのPRはありません！"
 
+    completeForReviewTitle = ":white_check_mark: レビューが完了しているPR"
+    if complete_prs:
+        completePrsText = "\n".join(waiting_prs)
+    else:
+        completePrsText = "レビュー待ちのPRはありません！"
+
+    text = f"{waitingForReviewTitle}\n{waitingPrsText}\n\n{completeForReviewTitle}\n{completePrsText}"
     # ペイロードを作成
     payload = {
         "text": text
@@ -120,47 +105,9 @@ def main():
             complete_prs=complete_prs,
             webhook_url=webhook_url
         )
-    # prs = fetch_prs()
-    # filtered_prs = filter_prs_by_label(prs, TARGET_LABEL)
-
-    # if not filtered_prs:
-    #     print(f"No PRs found with label '{TARGET_LABEL}'.")
-    #     exit(0)
-
-    # mergeable_prs = []
-    # pending_prs = []
-
-    # print("Checking 'mergeable_state' for PRs...")
-    # for pr in filtered_prs:
-    #     pr_url = pr["url"]
-    #     state = check_mergeable_state(pr_url)
-    #     if state == "mergeable":
-    #         mergeable_prs.append(pr["html_url"])
-    #     elif state == "pending":
-    #         pending_prs.append(pr["html_url"])
-
-    # print("Mergeable PRs URLs:")
-    # if mergeable_prs:
-    #     print("\n".join(mergeable_prs))
-    # else:
-    #     print("No mergeable PRs found.")
-
-    # print("Pending PRs (Not Ready for Merge) URLs:")
-    # if pending_prs:
-    #     print("\n".join(pending_prs))
-    # else:
-    #     print("No pending PRs found.")
 
 if __name__ == "__main__":
     main()
 
 
 # オブジェクトで取り扱いたい
-# ドラフトはとりのぞく
-# json のリストの分だけ繰り返したい
-# mergeable が常にtrueになるので、マージ可能の判定にmergeable の利用はむずかしそう。/pulls/4/reviwersの人数が{指定人数}以上の場合にマージ可能と判断する
-
-# 1. PR一覧を取得
-# 2. ラベルが指定されたPRを取得
-# 3. レビュアーが指定された人数以上のPRを取得
-# 4. slack 通知ようの形に整形
